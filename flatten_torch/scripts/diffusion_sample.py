16f7693f8bd78af69196595f1f8d6a3d2408c93f
Alex Nichol <unixpickle@gmail.com>
2023-05-29 09:04:36 -0700

    look at outputs from model

---
+++ flatten_torch/scripts/diffusion_sample.py
@@ -0,0 +1,55 @@
+import os
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from flatten_torch.data import Batch
+from flatten_torch.gaussian_diffusion import diffusion_from_config
+from flatten_torch.model import DiffusionPredictor
+
+LOAD_PATH = "diffusion_model.pt"
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = DiffusionPredictor(device=device)
+    diffusion = diffusion_from_config(
+        dict(
+            schedule="linear",
+            timesteps=1024,
+            respacing=128,
+        )
+    )
+    with open(LOAD_PATH, "r") as f:
+        obj = torch.load(f, map_location=device)
+        model.load_state_dict(obj["model"])
+
+    # Test input, should be from origin (0.3, 0.3, 0), size 0.15, rotation y 0.1, camera x -1
+    input = torch.tensor(
+        [
+            0.28982110038313036,
+            0.2912762684788842,
+            0.42850143149999603,
+            0.28710193481469054,
+            0.42850143149999603,
+            0.43065290222203584,
+            0.28982110038313036,
+            0.4369144027183263,
+        ],
+        device=device,
+    )
+
+    sample = diffusion.p_sample_loop(
+        model,
+        shape=(len(input), model.d_input),
+        clip_denoised=False,
+        model_kwargs=dict(cond=input),
+    )
+
+    origin, size, rotation, translation = torch.split(sample, [3, 2, 3, 3], dim=-1)
+    print(f"{origin=} {size=} {rotation=} {translation=}")
+
+
+if __name__ == "__main__":
+    main()
